bce9510cc03c9741369d01fccf9c091ae03c22a2
unknown <dejan.obr@hotmail.com>
2021-01-08 12:54:52 +0100

    implementing more functionality, making helper functions

--- src/components/panel-screen/Panel-screen.component.js
+++ src/components/panel-screen/Panel-screen.component.js
@@ -6,61 +6,96 @@ import { setScreenMessage, resetInputSequence, setIsLocked, setBoxCode, setScree
 import { screenMessages } from '../../constants';
 
 const PanelScreen = ({ screenMessage, inputSequence, isLocked, setScreenMessage, resetInputSequence, setIsLocked, setBoxCode, boxCode, setScreenActive, isActive }) => {
+	
+	//checking locking sequence (locking sequence should containt 6 digits and 7th character 'L')
+	function checkLockingSequence(sequence){
+		if(sequence[sequence.length - 1] === 'L' && sequence.length === 7 && typeof +sequence.substring(0, 6) === 'number')
+			return true;
+		return false;
+	}
 
+	//checking unlocking sequence (unlocking sequence should containt 6 digits)
+	function checkUnlockingSequence(sequence, boxCode){
+		if(sequence.length === 6 && typeof +sequence === 'number' && sequence === boxCode)
+			return true;
+		return false;
+	}
+
+	//save boxcode, change lock status and update screen messages
+	function saveBoxCode(){
+		const boxPass = inputSequence.substring(0, 6);
+		setBoxCode(boxPass);
+		resetInputSequence();
+		setScreenMessage(screenMessages.locking);
+		setTimeout(() => {
+			setIsLocked();
+			setScreenMessage(screenMessages.ready);
+			setTimeout( () => {
+				setScreenMessage(screenMessages.blank);
+			}, 500);
+		}, 3000);
+	}
+
+	//unlock box, change islocked, message and box code state 
+	function unlockBox(){
+		resetInputSequence();
+		setScreenMessage(screenMessages.unlocking);
+		setTimeout(() => {
+			setBoxCode('');
+			setIsLocked();
+			setScreenMessage(screenMessages.ready);
+			setTimeout( () => {
+				setScreenMessage(screenMessages.blank);
+			}, 500);
+		}, 3000);
+	}
+
+	//reject input sequence for locking/unlocking and show error on the screen
+	function rejectSequence(){
+		resetInputSequence();
+		setScreenMessage(screenMessages.error);
+		setTimeout( () => {setScreenMessage(screenMessages.blank);}, 1000);
+	}
+
+	//render last 10digit if code is longer than 10 characters, if sequence is for locking dont show character L on the end
+	function renderSequence(sequence){
+		if(!sequence.length)
+			return '';
+		if(sequence > 10)
+			sequence = sequence.substring(sequence.length-10);
+		if(sequence[sequence.length - 1] === 'L' && !isLocked)
+			return sequence.substring(0, sequence.length - 1);
+		return sequence;
+	}
+	
+	//if there is input sequence and sequence doesnt change for 1.2 s, code will be proccesed for unlocking/locking
 	React.useEffect( () => {
 		if(inputSequence && !isActive)
 			setScreenActive(true);
 
 		const timeoutId = setTimeout( () => {
 			if(inputSequence){
-				if(inputSequence.length === 6){
-					if(!isLocked){
-						setBoxCode(inputSequence);
-						resetInputSequence();
-						setScreenMessage(screenMessages.locking);
-						setTimeout(() => {
-							setIsLocked();
-							setScreenMessage(screenMessages.ready);
-							setTimeout( () => {
-								setScreenMessage(screenMessages.blank);
-							}, 500);
-						}, 3000);
+				if(!isLocked){
+					if(checkLockingSequence(inputSequence)){
+						saveBoxCode();
 					}
 					else{
-						if(inputSequence === boxCode){
-							resetInputSequence();
-							setScreenMessage(screenMessages.unlocking);
-							setTimeout(() => {
-								setBoxCode('');
-								setIsLocked();
-								setScreenMessage(screenMessages.ready);
-								setTimeout( () => {
-									setScreenMessage(screenMessages.blank);
-								}, 500);
-							}, 3000);
-
-						}
-						else{
-							resetInputSequence();
-							setScreenMessage(screenMessages.error);
-							setTimeout( () => {setScreenMessage(screenMessages.blank);}, 1000);
-						}		
+						rejectSequence();
 					}
-				}
-				else {
-					resetInputSequence();
-					setScreenMessage(screenMessages.error);
-					setTimeout( () => {setScreenMessage(screenMessages.blank);}, 1000);
+				} else {
+					if(checkUnlockingSequence(inputSequence, boxCode)){
+						unlockBox();
+					}
+					else{
+						rejectSequence();
+					}
 				}
 			}
 		}, 1200);
 
-		
-
 		return () => {
 			clearTimeout(timeoutId);
 		};
-
 
 	}, [inputSequence]);
 
@@ -79,7 +114,7 @@ const PanelScreen = ({ screenMessage, inputSequence, isLocked, setScreenMessage,
 	return (
 		<section className={`panel-screen ${isActive ? 'light-on' : 'light-off'}`}>
 			<span className="panel-screen__left">{ isLocked ? 'Locked' : 'Unlocked' }</span>
-			<span className="panel-screen__main">{ inputSequence || screenMessage }</span>
+			<span className="panel-screen__main">{ renderSequence(inputSequence) || screenMessage }</span>
 		</section>
 	);
 };
